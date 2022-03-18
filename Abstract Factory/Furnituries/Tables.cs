59b7c97d01e261556a2baa5571078101ff5e8231
Caraseni-Dumitras <90389146+Caraseni-Dumitras@users.noreply.github.com>
2022-03-18 17:16:55 +0200

    Add files via upload

---
+++ Abstract Factory/Furnituries/Tables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Furnituries
+{
+    internal class Tables: Furniture
+    {
+        public Tables()
+        {
+            Price = 750;
+        }
+
+        public override void Furniture_Info()
+        {
+            Console.WriteLine($"Table price is {Price}!");
+        }
+    }
+}
