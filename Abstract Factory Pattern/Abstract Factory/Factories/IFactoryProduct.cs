b00446c056967b7fbea779538d024ff5ed503ec8
Caraseni-Dumitras <90389146+Caraseni-Dumitras@users.noreply.github.com>
2022-03-18 17:21:13 +0200

    Add files via upload

---
+++ Abstract Factory Pattern/Abstract Factory/Factories/IFactoryProduct.cs
@@ -0,0 +1,16 @@
+﻿using AbstractFactory.Furnituries;
+using AbstractFactory.Styles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Factories
+{
+    internal interface IFactoryProduct
+    {
+        Furniture CreateFurniture();
+        Style CreateStyle();
+    }
+}
