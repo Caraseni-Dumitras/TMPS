7528ecc0c78ad2a694fd38182277f476a7bc2673
Caraseni-Dumitras <90389146+Caraseni-Dumitras@users.noreply.github.com>
2022-03-18 17:18:41 +0200

    Add files via upload

---
+++ Abstract Factory/Abstract Factory/Furnituries/Furniture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Furnituries
+{
+    abstract class Furniture
+    {
+        public int Price { get; set; }
+        public abstract void Furniture_Info();
+        
+    }
+}
