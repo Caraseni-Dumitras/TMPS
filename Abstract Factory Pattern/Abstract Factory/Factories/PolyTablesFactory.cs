08e01055fcb2c7b295c12a3b0d15523ddcf51149
Caraseni-Dumitras <90389146+Caraseni-Dumitras@users.noreply.github.com>
2022-03-18 17:27:06 +0200

    Add files via upload

---
+++ Abstract Factory Pattern/Abstract Factory/Factories/PolyTablesFactory.cs
@@ -0,0 +1,24 @@
+﻿using AbstractFactory.Factories;
+using AbstractFactory.Furnituries;
+using AbstractFactory.Styles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Factories
+{
+    internal class PolyTablesFactory : IFactoryProduct
+    {
+        public Furniture CreateFurniture()
+        {
+            return new Tables();
+        }
+
+        public Style CreateStyle()
+        {
+            return new Poly();
+        }
+    }
+}
---
+++ Abstract Factory Pattern/Abstract Factory/Factories/RetroStorageFactory.cs
@@ -0,0 +1,24 @@
+﻿using AbstractFactory.Factories;
+using AbstractFactory.Furnituries;
+using AbstractFactory.Styles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Factories
+{
+    internal class RetroStorageFactory : IFactoryProduct
+    {
+        public Furniture CreateFurniture()
+        {
+            return new Storage();
+        }
+
+        public Style CreateStyle()
+        {
+            return new Retro();
+        }
+    }
+}
